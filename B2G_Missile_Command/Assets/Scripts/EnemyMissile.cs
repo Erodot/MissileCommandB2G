8910acee680d55c8be7493826d3c732ec610e8b3
julien machado <julienmachado230@gmail.com>
2020-10-21 17:35:28 +0200

    enemy spawn and enemy deplacement

---
+++ B2G_Missile_Command/Assets/Scripts/EnemyMissile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMissile : MonoBehaviour
+{
+
+    GameObject[] targets;
+    Transform target;
+    [Range(0.0f, 10.0f)]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Player");
+        target = targets[Random.Range(0, targets.Length)].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ B2G_Missile_Command/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    Vector3 screenPos;
+
+    [SerializeField]
+    GameObject Enemy;
+
+    int enemyToSpawn;
+    [Range(0, 50)]
+    public int enemyMin;
+    [Range(0, 50)]
+    public int enemyMax;
+    [Range(0.0f, 5.0f)]
+    public float timeToSpawn;
+    float actualTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyToSpawn = Random.Range(enemyMin, enemyMax);
+        actualTime = timeToSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyToSpawn > 0)
+        {
+            if(actualTime == timeToSpawn)
+            {
+                InstantiateEnemy();
+            }
+
+            if(actualTime > 0)
+            {
+                actualTime -= Time.deltaTime;
+            }
+            else
+            {
+                actualTime = timeToSpawn;
+            }
+        }
+    }
+
+    void InstantiateEnemy()
+    {
+        screenPos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Screen.height, 10));
+
+        GameObject go = Instantiate(Enemy, screenPos, Quaternion.identity);
+
+        enemyToSpawn -= 1;
+    }
+
+
+}
