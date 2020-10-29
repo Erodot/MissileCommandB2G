e56ea387e4e20a9ad91cf2010c6f6bc3504b1cc1
unknown <fagipart@gmail.com>
2020-10-29 18:55:19 +0100

    Add rotation support for FieldBuilderTest.cs + New scene for testing spawner around a planet : CircleSpawnAlliesComponentTest.unity | SABIAUX Corentin GCC2

--- B2G_Missile_Command/Assets/Scripts/SpawnAlliesComponentTest/FieldBuilderTest.cs
+++ B2G_Missile_Command/Assets/Scripts/SpawnAlliesComponentTest/FieldBuilderTest.cs
@@ -10,6 +10,7 @@ public class FieldBuilderTest : MonoBehaviour //Script by Corentin SABIAUX GCC2,
     public GameObject Turret;
 
     public List<Vector3> positionTurretList = new List<Vector3>(); //Size = turretNumbers
+    public List<Vector3> rotationTurretList = new List<Vector3>(); //Size = turretNumbers
     private List<GameObject> TurretList = new List<GameObject>(); //Internal Use.
 
     public int cityNumbers; //We need 6 to fit the original game.
@@ -17,6 +18,7 @@ public class FieldBuilderTest : MonoBehaviour //Script by Corentin SABIAUX GCC2,
     public GameObject City;
 
     public List<Vector3> positionCityList = new List<Vector3>(); //Size = cityNumbers
+    public List<Vector3> rotationCityList = new List<Vector3>(); //Size = cityNumbers
     private List<GameObject> CityList = new List<GameObject>(); //Internal Use.
 
     void Start()
@@ -29,10 +31,10 @@ public class FieldBuilderTest : MonoBehaviour //Script by Corentin SABIAUX GCC2,
         //Let's create some turrets.
 
         GameObject TurretCategorie = new GameObject("Turrets"); //We create a category into the Unity Scene for better management.
-        TurretCategorie.transform.parent = transform.parent; //We set this category as a child of the field.
+        TurretCategorie.transform.parent = transform; //We set this category as a child of the field.
         for (int i = 0; i < turretNumbers; i++) //How many turrets do you want ?
         {
-            GameObject TurretCreated = Instantiate(Turret, positionTurretList[i], Quaternion.identity); //A new turret is born.
+            GameObject TurretCreated = Instantiate(Turret, positionTurretList[i], Quaternion.Euler(rotationTurretList[i])); //A new turret is born.
             TurretCreated.transform.parent = TurretCategorie.transform; //Turret became children of spawner.
             TurretList.Add(TurretCreated);
             TurretCreated.name = "IGTurret " + (i + 1); //Set turret name.
@@ -42,10 +44,10 @@ public class FieldBuilderTest : MonoBehaviour //Script by Corentin SABIAUX GCC2,
         //Let's create some cities.
          
         GameObject CityCategorie = new GameObject("Cities"); //We create a category into the Unity Scene for better management.
-        CityCategorie.transform.parent = transform.parent; //We set this category as a child of the field.
+        CityCategorie.transform.parent = transform; //We set this category as a child of the field.
         for (int i = 0; i < cityNumbers; i++) //How many cities do you want ?
         {
-            GameObject CityCreated = Instantiate(City, positionCityList[i], Quaternion.identity); //A new city is born.
+            GameObject CityCreated = Instantiate(City, positionCityList[i], Quaternion.Euler(rotationCityList[i])); //A new city is born.
             CityCreated.transform.parent = CityCategorie.transform; //City became children of spawner.
             CityList.Add(CityCreated);
             CityCreated.name = "IGCity " + (i + 1); //Set City name | Welcome to City 17.
