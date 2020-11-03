ee6781d9fa2d606b42c9615658eb8ebc6520d757
unknown <fagipart@gmail.com>
2020-11-03 20:31:51 +0100

    Add planet moving by rotation support with PlanetController.cs | SABIAUX Corentin GCC2

---
+++ B2G_Missile_Command/Assets/Scripts/SpawnAlliesComponentTest/PlanetControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetControllerTest : MonoBehaviour //Script by Corentin SABIAUX GCC2, don't hesitate to ask some questions.
+{
+    public CharacterController controller; //We need the controller from the gameObject field.
+    public Transform fieldTransform; //We need the transform from the gameObject field.
+
+    public float rotationSpeed = 1f; //Here, you can choose the speed of rotation.
+    private float horizontalMove = 0f;
+
+    public bool isRotationAccurate; //Internal use, check-it if you want to have a more accurate rotation of the planet.
+
+    // Update is called once per frame
+    void Update()
+    {
+        fieldTransform.rotation = Quaternion.Euler(0, 0, horizontalMove); //Rotation is refreshed every single frame | Rotation at Z axis.
+
+        if (isRotationAccurate == true)
+        {
+            horizontalMove += Input.GetAxisRaw("Horizontal") * rotationSpeed; //The planet is rotating accurately by horizontal positive and negative button.
+        } else
+        {
+            horizontalMove += Input.GetAxis("Horizontal") * rotationSpeed; //The planet is rotating smoothly by horizontal positive and negative button.
+        }
+        
+        
+    }
+}
