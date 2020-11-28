861f39ca778273e47dabe78109a39e27f127af61
Erodot <tom.theodore0309@gmail.com>
2020-11-28 13:24:38 +0100

    integration Scene menu et paramètre //Attention encore en cours de developpement

---
+++ B2G_Missile_Command/Assets/01_SCRIPTS/General/SettingsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject GeneralPanel;
+    public GameObject SettingsPanel;
+
+    public AudioMixer audioMixer;
+
+    //General Menu Command
+    public void PlayButton()
+    {
+        //SceneManager.LoadScene("Name Your Scene here", LoadSceneMode.Single);
+    }
+
+    public void SettingsButton()
+    {
+        GeneralPanel.gameObject.SetActive(false);
+        SettingsPanel.gameObject.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    //Settings Menu Command
+
+    //Call MainAudioMixer//
+    public void SetVolume(float volume)
+    {
+        //Caution don't touch VolumeSlider Min and Max Value//
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void ReturnButton()
+    {
+        SettingsPanel.gameObject.SetActive(false);
+        GeneralPanel.gameObject.SetActive(true);
+    }
+}
