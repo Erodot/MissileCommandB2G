47a0a9fec27fc411727bab908642762f1471dd51
Erodot <tom.theodore0309@gmail.com>
2020-12-02 10:07:34 +0100

    Menu(Volume/Resolution/Fullscreen)

--- B2G_Missile_Command/Assets/01_SCRIPTS/General/SettingsMenu.cs
+++ B2G_Missile_Command/Assets/01_SCRIPTS/General/SettingsMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -10,6 +11,51 @@ public class SettingsMenu : MonoBehaviour
     public GameObject SettingsPanel;
 
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        //Display the list of disponible resolution
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            //Verify the selected current resolution
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    //Update the current resolution
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 
     //General Menu Command
     public void PlayButton()
